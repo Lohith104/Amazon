083892379d64ca8927df47fcdbcaa49004e20116
LohithKM <kmlohith4@gmail.com>
2022-09-30 19:43:43 +0530

    no javascript

--- test/java/com/Amazon/Amazon/Amazon_Automation.java
+++ test/java/com/Amazon/Amazon/Amazon_Automation.java
@@ -40,8 +40,7 @@ public class Amazon_Automation extends Regular_methods {
 		clickelement(sdp.getHm().getApple());
 		clickelement(sdp.getHm().getA_phone());
 		Thread.sleep(2000);
-		// js.executeScript("arguments[0].scrollIntoView();",sdp.getHm().getTB());
-		Thread.sleep(5000);
+				Thread.sleep(5000);
 		Actions("movetoelement", sdp.getHm().getButton());
 		
 		
